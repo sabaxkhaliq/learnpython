56fd7a4c23aaf4adf81fc3d3a2d93ca9b2738a0e
sabax <sabaxkhaliq@gmail.com>
2026-06-06 13:33:04 +0500

    Python Project

--- company.py
+++ company.py
@@ -22,6 +22,14 @@ def company_info(data):
 
     print("\nThe other Branch is in ", data["company"]["offices"]["branches"][1]["city"],"\nThe Total Employees is :: ",data["company"]["offices"]["branches"][1]["employees_count"],"\n") 
 
+def update_comp(data):
+    print("Company Name :: ",data["company"]["name"] )
+    new_name = input("Enter the Company New Name :: ")
+    data["company"]["name"] = new_name
+    print("Company Name Saved Successfully....")
+    
+    save_data(data)
+
 def employee_info(data):
     
     print("Employee Name is :: ",data["company"]["employees"][0]["name"],"\nskills is ", data["company"]["employees"][0]["skills"][0],",",data["company"]["employees"][0]["skills"][1],"and",data["company"]["employees"][0]["skills"][2])
@@ -36,7 +44,18 @@ def employee_info(data):
 
     print("Project is ", data["company"]["employees"][1]["projects"][0]["name"],"and it is ",data["company"]["employees"][1]["projects"][0]["status"], "\n")
 
+def update_emp(data):
+    user_id = int(input("Enter The ID of Employee :: "))
+    employee_list = data["company"]["employees"]
 
+    for employee in employee_list:
+        if employee["id"] == user_id:
+            print("Employee Name :: ",employee["name"])
+            new_name = input("Enter the Employee New Name :: ")
+            employee["name"] = new_name
+            print("Employee Name Saved Successfully....")
+    
+    save_data(data)
 
 def main():
     data = load_data()
@@ -46,10 +65,26 @@ def main():
         choice = int(input("Enter Your Choice :: "))
 
         if choice == 1:
-            company_info(data)
+            print("1.View Company info\n2.Update Company info")
+            choose = int(input("Enter Your Choice :: "))
+
+            if choose == 1:
+                company_info(data)
+            elif choose == 2:
+                update_comp(data)
+            else :
+                print("Invalid choice")
 
         elif choice == 2:
-            employee_info(data)
+            print("1.View Employee info\n2.Update Employee info")
+            var = int(input("Enter Your Choice :: "))
+
+            if var == 1:
+                employee_info(data)
+            elif var == 2:
+                update_emp(data)
+            else :
+                print("Invalid choice")
 
         elif choice == 3:
             print("Exit")
