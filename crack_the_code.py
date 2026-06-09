79239d407b8e90bd61bf121a52c633897fba4fd2
sabax <sabaxkhaliq@gmail.com>
2026-06-09 13:20:12 +0500

    Crack_Code

---
+++ crack_the_code.py
@@ -0,0 +1,14 @@
+
+code = 1234
+
+while(1):
+    num = int(input("Enter The Correct Code :: "))
+    if num == code:
+        print("This code is Correct")
+        exit()
+    else:
+        print("Wrong code \nTry Again")
+
+
+
+
