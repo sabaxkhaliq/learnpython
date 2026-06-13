2a2359ad74070efbbfc29cac8adaf4a6c6894703
sabax <sabaxkhaliq@gmail.com>
2026-06-13 13:31:37 +0500

    Operators

---
+++ relational.py
@@ -0,0 +1,8 @@
+num = input("Enter the Number :: ")
+num2 = input("Enter the Number :: ")
+
+print(num, ">", num2, "=", num > num2)
+print(num, "<", num2, "=", num < num2)
+print(num, "==", num2, "=", num == num2)
+print(num, ">=", num2, "=", num >= num2)
+print(num, "<=", num2, "=", num <= num2)
