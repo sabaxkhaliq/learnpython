8fb4ed567cf00bfc2917384dd0e1b9b7aa3de676
sabax <sabaxkhaliq@gmail.com>
2026-07-30 19:07:03 +0500

    Python Project

---
+++ mini_projects/list.py
@@ -0,0 +1,8 @@
+fruit_list = ["Mango", "Apple", "Banana"]
+
+fruit_list[0] = "Orange"
+fruit_list.append("Strawberry")
+fruit_list.append("Grapes")
+
+print(fruit_list)
+print("The Total Number of Fruits is :: ", len(fruit_list))
