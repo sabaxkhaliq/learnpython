2bb1bc9d15c2d1f8a02b890903a5c904c429c77e
sabax <sabaxkhaliq@gmail.com>
2026-09-16 14:40:18 +0500

    Vowels

---
+++ smallcode/vowel.py
@@ -0,0 +1,8 @@
+ch = input("Enter any Character ::")
+
+if (ch == 'A' or ch == 'a' or ch == 'E' or ch == 'e' or ch == 'I' or ch == 'i' or ch == 'O' or ch == 'o' or ch == 'U' or ch == 'u'):
+    print("You Entered a Vowel :: ", ch)
+    
+else :
+    print("You did not enter a Character :: ", ch)
+    
