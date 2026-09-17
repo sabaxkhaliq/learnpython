20a9298c1a11a8b7296267c10560c70c3074bb3f
sabax <sabaxkhaliq@gmail.com>
2026-09-17 14:25:28 +0500

    Function

---
+++ mini_projects/function.py
@@ -0,0 +1,12 @@
+def show(x = "Python"):
+
+    print(x)
+
+def main():
+    show("Welcome to Programming Language")
+    show("C")
+    show()
+    
+    
+if __name__ == "__main__":
+        main()
