91296e519af368c337464a5bc9ff309744c64de5
sabax <sabaxkhaliq@gmail.com>
2026-09-20 15:49:09 +0500

    Weather

---
+++ smallcode/if_else/weather.py
@@ -0,0 +1,11 @@
+weather = input("Enter the Number :: ")
+
+if weather == "sunny" :
+    print("Wear Sunglasses")
+    
+elif weather == "rainy":
+    print("Take an Umbrella")
+
+else :
+    print("Enjoy your Day...! ")
+    
