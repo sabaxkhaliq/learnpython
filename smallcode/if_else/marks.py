3a7876a4a29d48c39264e25955c224afb20ea4a8
sabax <sabaxkhaliq@gmail.com>
2026-09-21 14:29:53 +0500

    Marks

---
+++ smallcode/if_else/marks.py
@@ -0,0 +1,8 @@
+marks = int(input("Enter your Marks :: "))
+
+if marks >= 40 :
+    print("You have Passed...")
+    print("Congratulations!")
+else:
+    print("You have Failed")
+    print("Try Again...!")
