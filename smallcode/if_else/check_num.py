3cdbdcd82357a4132aa79f3d32eaf41343a54bff
sabax <sabaxkhaliq@gmail.com>
2026-09-23 15:18:00 +0500

    Check_num

---
+++ smallcode/if_else/check_num.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the Number :: "))
+
+if num > 0:
+    print("The Number is Positive")
+elif num < 0:
+    print("The Number is Negative")
+else:
+    print("The Number is Zero") 
